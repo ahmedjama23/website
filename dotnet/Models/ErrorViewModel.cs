1e667200240b8c4ec73fdf855ad4dcada3ef98a8
“Ahmed <ajama07@hotmail.com~>
2022-04-02 10:14:41 -0600

    init

---
+++ dotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace dotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
