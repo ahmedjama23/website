1eddfeb03b643e6576907e11a92dffec32b089cf
“Ahmed <ajama07@hotmail.com~>
2022-04-05 20:38:33 -0600

    Removed responsibilities from job repository

--- dotnet/Models/Jobs/IJob.cs
+++ dotnet/Models/Jobs/IJob.cs
@@ -9,7 +9,6 @@ namespace dotnet.Models.Jobs
         public string Company { get; set; }
         public string Position { get; set; }
         public string Locale { get; set; }
-        public List<string> Responsibilities { get; set; }
         public string Description { get; set; }
         public string BeginDate { get; set; }
         public string EndDate { get; set; }
